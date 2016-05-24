190e3b3cdc2bf2b3a1c4bdc0a30db074c4cae6d9
Lessar277 <lessar277@gmail.com>
2016-05-25 02:38:56 +0300

    updated app with ajax functionality

---
+++ js/script.js
@@ -0,0 +1,85 @@
+//function to load the CSS icons
+function loadSrc(src) {
+    $('.weather-icon').load(src);
+}
+//options for the API call, it will get populated with units and city
+var options = {
+    "appid": "7f6435e69b025332b6bbf79e3a160058"
+}
+
+
+
+function toCelsius(temp) {
+    return Math.round((5/9) * (temp-32));
+}
+function toFahrenheit(temp) {
+    return Math.round(temp * 9 / 5 + 32);
+}
+
+function tempSymbol(units) {
+    if (units === 'imperial') {
+        $('.symbol').html(' &#8457')
+    } else {
+        $('.symbol').html(' &#8451')
+    }
+}
+
+$('#metric').click(function() {
+    var temp = Number($('.temp').text());
+    if ($(this).hasClass('active')) {
+        return
+    }
+    $('#imperial').removeClass('active');
+    $(this).addClass('active');
+    options.units = 'metric';
+    $('.temp').text(toCelsius(temp));
+    tempSymbol('metric');
+})
+
+$('#imperial').click(function() {
+    var temp = Number($('.temp').text());
+    if ($(this).hasClass('active')) {
+        return
+    }
+    $('#metric').removeClass('active');
+    $(this).addClass('active');
+    options.units = 'imperial';
+    $('.temp').text(toFahrenheit(temp));
+    tempSymbol('imperial');
+})
+
+
+function getWeather() {
+    options.q = $('#input').val();
+    options.units = options.units || 'metric';
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather', options, function(data) {
+        console.log(data.weather[0].id);
+        $('.city').html(data.name);
+        $('.temp').html(Math.round(data.main.temp));
+        tempSymbol(options.units)
+        if(data.weather[0].id === 800) {
+            loadSrc('sun.html')
+        } else {
+            loadSrc('thunder-storm.html')
+        }
+    })
+}
+
+
+$('#search').click(getWeather);
+$('#input').keypress(function(e) {
+    if (e.which === 13) {
+        getWeather();
+    }
+})
+
+
+$(document).ready(function() {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+                options.lat = position.coords.latitude;
+                options.lon = position.coords.longitude;
+                getWeather();
+        })
+    }
+});
